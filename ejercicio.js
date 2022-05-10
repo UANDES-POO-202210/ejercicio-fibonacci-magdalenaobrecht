61484613f9e0f8a60f877c92bc66e0d62a9bc6b8
Magdalena Obrecht <mpobrecht@miuandes.cl>
2022-05-10 00:49:53 -0400

    version actualizada de latarea

--- ejercicio.js
+++ ejercicio.js
@@ -10,10 +10,10 @@ fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
 
 function fibonacci(n){
   if (n==0){
-    return 0;
+    return n;
   }
   else if (n==1){
-    return 1;
+    return n;
   }
   if (n>1){
     return fibonacci(n-1)+fibonacci(n-2)
